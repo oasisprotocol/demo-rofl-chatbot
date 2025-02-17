b05e70fa7a1524daab5555a2eacabe3380f5939e
Matevž Jekovec <matevz@oasisprotocol.org>
2025-02-17 15:11:08 +0100

    contracts: Pin solc version to 0.8.24

--- contracts/src/ChatBot.sol
+++ contracts/src/ChatBot.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
 import {Subcall} from "@oasisprotocol/sapphire-contracts/contracts/Subcall.sol";
 import {SiweAuth} from "@oasisprotocol/sapphire-contracts/contracts/auth/SiweAuth.sol";
